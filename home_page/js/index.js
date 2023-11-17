9b1724fe09a137aa7e49bf36b7b17221f3ba5f65
Aaron Barbiero <124076675+nicespiced@users.noreply.github.com>
2023-11-16 21:16:38 -0500

    Add files via upload
    more things and stuff 💀💀🤠🤠

--- home_page/js/index.js
+++ home_page/js/index.js
@@ -2,3 +2,15 @@ function toDark() {
     let element = document.body
     element.classList.toggle("dark")
 }
+
+function toHome() {
+    document.getElementById("home").onclick = function () { location.href = "/home_page/html/home.html"}
+}
+
+function toRules() {
+    document.getElementById("rules").onclick = function () { location.href = "/home_page/html/rulesets.html"}
+}
+
+function createGame() {
+    document.getElementById("popup1").classList.toggle("active")
+}
