c4cfc26665f502efd95f533e52d740e24bbd08f4
Mitchell DeCesare <98658542+MitchellDC@users.noreply.github.com>
2023-11-18 16:03:32 -0500

    Update signup.js

--- login-signup/js/signup.js
+++ login-signup/js/signup.js
@@ -1,4 +1,23 @@
 // Sign-Up Page
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+const conn = mysql.createConnection({
+    host: '34.73.177.149',
+    user: 'mitchelldc22',
+    password: 'teaminfinitetime330',
+    database: 'Project',
+});
+
+
 function queryObjectToString(query) {
     let properties = Object.keys(query);
     let arrOfQuesryStrings = properties.map(prop => prop+"="+query[prop]);
@@ -39,11 +58,60 @@ function extra(obj){
 			}
 		else{
 			// SUCCESSFULLY CREATED
-			console.log("new account created");
-			db.push(obj);
-			console.log(db);
-			document.getElementById("result").innerHTML = "Account Created!";
+			// console.log("new account created");
+			// db.push(obj);
+			// console.log(db);
+			// document.getElementById("result").innerHTML = "Account Created!";
+			conn.connect(function(err) {
+				if (err) {
+					console.log("Error connecting to the database: " + err.message);
+				} else {
+					console.log("Connected to the database");
+			
+					app.use(bodyParser.json());
+			
+					app.post("/submit", (req, res) => {
+						const { username, email, password } = req.body;
+			
+						const sql = "INSERT INTO Player (Player_ID, Username, Email, Password, Games_Played, Games_Won, Ongoing_Games, Is_Admin) VALUES ("+uname.value+","+email.value+","+pword.value+", NULL, NULL, NULL, NULL)";
+			
+						conn.query(sql, [username, email, password], (err, result) => {
+							if (err) {
+								console.log("Error executing query: " + err.message);
+								res.status(500).json({ error: "Internal Server Error" });
+							} else {
+								console.log("Data is inserted successfully");
+								res.status(200).json({ success: "Data inserted successfully" });
+							}
+						});
+					});
+			
+					app.listen(port, () => {
+						console.log(`Server listening on port ${port}`);
+					});
+				}
+			});
+			// app.use(express.json()); 
 
+			// app.get('/', (req, res) => {
+			// 	res.sendFile(__dirname + '/signup.html');
+			// });
+			
+			// app.post('/', (req, res) => {
+			// 	const {uname, email, pword} = req.body; 
+			// 	const {authorization} = req.headers; 
+			// 	res.send({
+			// 		uname, 
+			// 		email, 
+			// 		pword, 
+			// 		authorization,
+			// 	}); 
+			// }); 
+			
+			// app.listen(3000, () => {
+			// 	console.log("port 3000"); 
+			// });
+			
 			}
 	}
 }
