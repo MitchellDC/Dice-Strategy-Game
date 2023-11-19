9a7c34016cf727b5eb1dd99c214e822dfc3b77ff
Dom Carrillo <carrillod5@southernct.edu>
2023-11-19 20:08:46 +0000

    implemented database on signup page

--- login-signup/js/signup.js
+++ login-signup/js/signup.js
@@ -1,35 +1,39 @@
 // Sign-Up Page
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
-const express = require('express'); 
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-
-const app = express();
-const port = 3000;
-
-const conn = mysql.createConnection({
-    host: '34.73.177.149',
-    user: 'mitchelldc22',
-    password: 'teaminfinitetime330',
-    database: 'Project',
-});
-
-
 function queryObjectToString(query) {
     let properties = Object.keys(query);
     let arrOfQuesryStrings = properties.map(prop => prop+"="+query[prop]);
     return(arrOfQuesryStrings.join('&'));
  }
 
+function clearFields(){
+	document.getElementById("email").value = "";
+	document.getElementById("uname").value = "";
+	document.getElementById("pword").value = "";
+	document.getElementById("vpass").value = "";
+}
+
 function extra(obj){
 	if(vpass.value != obj.pword){
 		document.getElementById("result").innerHTML = "Passwords do not match!";
         }
 	else{
+		console.log(obj.email)
+		if(obj.email=="found")
+		{
+			document.getElementById("result").innerHTML = "Account with your email already exists!";
+			clearFields();
+		}
+		else if(obj.uname=="found")
+		{
+			document.getElementById("result").innerHTML = "Account with your username already exists!";
+			clearFields();
+		}
+		else{
+			alert("Account Successfully Created!");
+			window.open("http://35.231.124.196/signup.html","_self");
+		}
 		// LOOPING THROUGH THE OBJECT TO SEE IF THE TYPED IN EMAIL AND USERNAME ARE ALREADY IN THE DATABASE 
+		/*
 		let founduname = false;
 		let foundemail = false;
 		for(const i in db){
@@ -55,64 +59,16 @@ function extra(obj){
 				// TYPED IN  USERNAME IS ALREADY IN THE DATABASE
 				document.getElementById("result").innerHTML = "Username already taken! Choose another one.";
 				}
-			}
+			
+			clearFields();
+		}
 		else{
 			// SUCCESSFULLY CREATED
-			// console.log("new account created");
+			console.log("new account created");
 			// db.push(obj);
 			// console.log(db);
-			// document.getElementById("result").innerHTML = "Account Created!";
-			conn.connect(function(err) {
-				if (err) {
-					console.log("Error connecting to the database: " + err.message);
-				} else {
-					console.log("Connected to the database");
-			
-					app.use(bodyParser.json());
-			
-					app.post("/submit", (req, res) => {
-						const { username, email, password } = req.body;
-			
-						const sql = "INSERT INTO Player (Player_ID, Username, Email, Password, Games_Played, Games_Won, Ongoing_Games, Is_Admin) VALUES ("+uname.value+","+email.value+","+pword.value+", NULL, NULL, NULL, NULL)";
-			
-						conn.query(sql, [username, email, password], (err, result) => {
-							if (err) {
-								console.log("Error executing query: " + err.message);
-								res.status(500).json({ error: "Internal Server Error" });
-							} else {
-								console.log("Data is inserted successfully");
-								res.status(200).json({ success: "Data inserted successfully" });
-							}
-						});
-					});
-			
-					app.listen(port, () => {
-						console.log(`Server listening on port ${port}`);
-					});
-				}
-			});
-			// app.use(express.json()); 
-
-			// app.get('/', (req, res) => {
-			// 	res.sendFile(__dirname + '/signup.html');
-			// });
-			
-			// app.post('/', (req, res) => {
-			// 	const {uname, email, pword} = req.body; 
-			// 	const {authorization} = req.headers; 
-			// 	res.send({
-			// 		uname, 
-			// 		email, 
-			// 		pword, 
-			// 		authorization,
-			// 	}); 
-			// }); 
-			
-			// app.listen(3000, () => {
-			// 	console.log("port 3000"); 
-			// });
-			
-			}
+			document.getElementById("result").innerHTML = "Account Created!";
+			}*/
 	}
 }
 
@@ -127,8 +83,8 @@ const sendAJAX = function(){
 	xmlhttp.onload = function() {
 		if (this.status == 200){
 			alert("it's working");
-			resultObject = JSON.parse(this.responseText);
-				
+			let resultObject = JSON.parse(this.responseText);
+
 			// ALL BLANKS WERE FILLED IN
 			if(resultObject.email && resultObject.uname && resultObject.pword)
 			{	console.log(vpass.value);
@@ -138,11 +94,13 @@ const sendAJAX = function(){
 				}
 				else{
 					 document.getElementById("result").innerHTML = "Verify your password!";
+					 clearFields();
 				}
 			}
 			// AT LEAST ONE BLANK WAS NOT FILLED IN
 			else{
 				document.getElementById("result").innerHTML = "Fill in everything!";
+				clearFields();
 			}
 		}
 		else{ //NOTHING WAS FILLED IN
@@ -152,8 +110,11 @@ const sendAJAX = function(){
 	xmlhttp.open("GET","http://35.231.124.196/signup?"+queryObjectToString({email:email.value,uname:uname.value,pword:pword.value}));
 	xmlhttp.send();
 }
+alert("keep going");
 document.getElementById("button").addEventListener("click",sendAJAX); // CREATE ACCOUNT BUTTON
 document.getElementById("lbutton").addEventListener("click",function() { //MOVING TO LOGIN PAGE WITHOUT NEEDING TO SIGN IN
 				window.open("http://35.231.124.196/login.html","_self");
 					});
 document.getElementById("result").innerHTML = "Create your account!";
+
+
