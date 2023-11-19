00366766d14f10fa3b72b39aa089946fcadda5df
Mitchell DeCesare <decesarem2@southernct.edu>
2023-11-19 20:14:20 +0000

    something

--- app.js
+++ app.js
@@ -2,109 +2,32 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const path = require('path');
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
-function home(req,res)
-{
-	console.log("yes");
-	let fileName = "html/home_page/html/home.html";
-        fs.readFile(fileName, function(err,data){
-
-        if(err){
-                res.writeHead(404,{'Content-Type': 'text/plain'});
-                res.write('Error 404: resource not found.');
-                res.end();
-                }
-        else {
-                responses(path.extname(fileName),res);
-                write(data,res);
-                }
-
-        });
-}
+const app = express();
+const port = 3000;
 
 
-function signIn(query,res){
-	//signIn code goes here
 
-}
+app.use(express.json()); 
 
-function signUp(query, res){
-	if(!query.email|| !query.uname || !query.pword){
-		res.writeHead(404,{'Content-Type': 'text/plain'});
-		res.write('Error 404: resource not found.');
-		}
-	else {
-		res.writeHead(200,{'Content-Type':'application/json'});
-		res.write(JSON.stringify({email:query.email,uname:query.uname,pword:query.pword}));
-		}
-	res.end();
-	}
-function responses(ext,res){
-	 switch(ext){
-		case '.jpg':
-			res.writeHead(200, {'Content-Type': 'image/jpg'});
-			break;
-                case '.gif':
-                        res.writeHead(200, {'Content-Type': 'image/gif'});
-                        break;
-                case '.png':
-                        res.writeHead(200, {'Content-Type': 'image/png'});
-                        break;     
-                case '.js':
-                        res.writeHead(200, {'Content-Type': 'text/javascript'});
-                        break;
-                case '.html':
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        break;
-                case '.css':
-                        res.writeHead(200, {'Content-Type': 'text/css'});
-                        break;
-                default:
-                        res.writeHead(200, {'Content-Type': 'text/plain'});
-                        break;
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/signup.html');
+});
 
-	}
-}
+app.post('/', (req, res) => {
+	const {uname, email, pword} = req.body; 
+	const {authorization} = req.headers; 
+	res.send({
+		uname, 
+		email, 
+		pword, 
+		authorization,
+	}); 
+}); 
 
-function write(data,res){
-	res.write(data);
-	res.end();
-}
-
-const sendFile = function(req,res){
-	let fileName = "html"+url.parse(req.url).pathname;
-	fs.readFile(fileName, function(err,data){
-
-	if(err){
-		res.writeHead(404,{'Content-Type': 'text/plain'});
-		res.write('Error 404: resource not found.');
-		res.end();
-		}
-	else {
-		responses(path.extname(fileName),res);
-		write(data,res);
-		}
-
-	});
-}
-
-const main = function(req, res){
-
-	let parsedURL = url.parse(req.url,true);
-	if (parsedURL.pathname=="/signup"){
-		return signUp(parsedURL.query,res);
-		}
-	else if(parsedURL.pathname=="/signin"){
-		return signIn(parsedURL.query,res);
-		}
-	else if(parsedURL.pathname=="/home"){
-		return home(req,res);
-		}
-	else
-	{
-		return sendFile(req,res);
-	}
-}
-
-const myserver = http.createServer(main);
-myserver.listen(80, function() {console.log("Listening on port 80")});
+app.listen(3000, () => {
+	console.log("port 3000"); 
+});
--- login-signup/js/signup.js
+++ login-signup/js/signup.js
@@ -1,91 +1,24 @@
 // Sign-Up Page
-function queryObjectToString(query) {
-    let properties = Object.keys(query);
-    let arrOfQuesryStrings = properties.map(prop => prop+"="+query[prop]);
-    return(arrOfQuesryStrings.join('&'));
- }
+const express = require('express'); 
+const app = express(); 
 
-function extra(obj){
-	if(vpass.value != obj.pword){
-		document.getElementById("result").innerHTML = "Passwords do not match!";
-        }
-	else{
-		// LOOPING THROUGH THE OBJECT TO SEE IF THE TYPED IN EMAIL AND USERNAME ARE ALREADY IN THE DATABASE 
-		let founduname = false;
-		let foundemail = false;
-		for(const i in db){
-			// USERNAME IS ALREADY IN THE DATABASE
-			if(obj.uname==db[i].uname){
-				founduname = true;
-				console.log("username already exists");
-			}
-			//EMAIL IS ALREADY IN THE DATABASE
-			if(obj.email==db[i].email){
-				foundemail = true;
-				console.log("email already exists");
-                         }
-		}
-		if(founduname || foundemail){
-			// ACCOUNT ALREADY EXITS
-			console.log("account exists");
-			if(foundemail){
-				// TYPED IN EMAIL IS ALREADY IN THE DATABASE
-				document.getElementById("result").innerHTML = "Account with your email already exists!";
-				}
-			else{
-				// TYPED IN  USERNAME IS ALREADY IN THE DATABASE
-				document.getElementById("result").innerHTML = "Username already taken! Choose another one.";
-				}
-			}
-		else{
-			// SUCCESSFULLY CREATED
-			console.log("new account created");
-			db.push(obj);
-			console.log(db);
-			document.getElementById("result").innerHTML = "Account Created!";
+app.use(express.json()); 
 
-			}
-	}
-}
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/signup.html');
+});
 
-// MADE THIS OBJECT BEFORE THE DATABASE
-let db = [{email:"carrillod5@southernct.edu",uname:"mongabcarrillo",pword:"password"}];
-console.log(db);
+app.post('/', (req, res) => {
+	const {uname, email, pword} = req.body; 
+	const {authorization} = req.headers; 
+	res.send({
+		uname, 
+		email, 
+		pword, 
+		authorization,
+	}); 
+}); 
 
-
-const sendAJAX = function(){
-	let xmlhttp = new XMLHttpRequest();
-	xmlhttp.onerror = function(){ alert("Error") };
-	xmlhttp.onload = function() {
-		if (this.status == 200){
-			alert("it's working");
-			resultObject = JSON.parse(this.responseText);
-				
-			// ALL BLANKS WERE FILLED IN
-			if(resultObject.email && resultObject.uname && resultObject.pword)
-			{	console.log(vpass.value);
-				if(vpass.value)
-				{
-					extra(resultObject);
-				}
-				else{
-					 document.getElementById("result").innerHTML = "Verify your password!";
-				}
-			}
-			// AT LEAST ONE BLANK WAS NOT FILLED IN
-			else{
-				document.getElementById("result").innerHTML = "Fill in everything!";
-			}
-		}
-		else{ //NOTHING WAS FILLED IN
-			alert("Fill in the blanks to create account!");
-			}
-		}
-	xmlhttp.open("GET","http://35.231.124.196/signup?"+queryObjectToString({email:email.value,uname:uname.value,pword:pword.value}));
-	xmlhttp.send();
-}
-document.getElementById("button").addEventListener("click",sendAJAX); // CREATE ACCOUNT BUTTON
-document.getElementById("lbutton").addEventListener("click",function() { //MOVING TO LOGIN PAGE WITHOUT NEEDING TO SIGN IN
-				window.open("http://35.231.124.196/login.html","_self");
-					});
-document.getElementById("result").innerHTML = "Create your account!";
+app.listen(3000, () => {
+	console.log("port 3000"); 
+});
