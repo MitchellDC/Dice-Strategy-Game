f5d947b643cb551a9064be285db1ce8cb24d99c7
Dom Carrillo <carrillod5@southernct.edu>
2023-12-03 05:55:39 +0000

    made the game accessible in the home page but without database for now

--- public_html/js/home.js
+++ public_html/js/home.js
@@ -22,10 +22,68 @@ function toRules() {
     document.getElementById("rules").onclick = function () { location.href = "rulesets.html"}
 }
 
+function showGames(){
+
+
+	// takes the game table from the html
+	let gameTable = document.getElementById("gameTable");
+
+	let xmlhttp = new XMLHttpRequest();
+	xmlhttp.onerror = function(){alert("Error!")};
+	xmlhttp.onload = function(){
+		if(this.status!=200){alert("Error!")}
+		else{
+
+			let resp = JSON.parse(this.responseText); // takes the response from the server in variable resp
+			if(resp.length>0){
+				document.getElementById("noGames").style.display="none"; //hides the table that says no games if user has ongoing games
+				for(game in resp){ //every game in the response is taken
+					if(resp[game].Player1_uname==username){ // takes the username of the other player in the game
+						opponent = resp[game].Player2_uname;
+					}
+					else{
+						opponent = resp[game].Player1_uname;
+					}
+					newRow = gameTable.insertRow(); // new row is made
+
+					newRow.classList.add("scroll"); // class 'scroll' is assigned to the new row
+					oppColumn = newRow.insertCell(0); // variable for the first column of the new row
+					ruleColumn = newRow.insertCell(1); // variable for the second column of the new row
+					turnColumn = newRow.insertCell(2); // variable for the third column of the new row
+
+					oppColumn.innerHTML = opponent; // labels the first column of the first row as the user name of the opponent
+					ruleColumn.innerHTML = "Standard" //displays the ruleset (will change once the rulesets are applied)
+					turnColumn.innerHTML = resp[game].totalturns; //displays the number of turns in the game, taken from the server
+
+					oppColumn.classList.add("ongoing"); //assigns class to the first column
+					oppColumn.id = "list"; //assigns id to the first column
+					ruleColumn.classList.add("details"); //assigns class to the second column
+					turnColumn.classList.add("details"); //assigns class to the third column
+
+					oppColumn.addEventListener("click", toGame); //brings user to the game if the opponent's username is clicked
+				}
+			}
+			else{
+				document.getElementById("noGames").style.display="block"; //another table that says no ongoing games is shown if no games are being played
+				document.getElementById("noGames").innerHTML="<b>NO ONGOING GAMES</b>"
+				gameTable.style.display='none'; //table is hidden if there are no ongoing games
+			}
+
+		}
+
+	}
+	xmlhttp.open("GET","http://35.231.124.196/games?"+queryObjectToString({uname:username}));
+	xmlhttp.send();
+
+
+
+}
+showGames();
+
 function createGame() {
     document.getElementById("popup1").classList.toggle("active")
 }
-
+/*
 function toNewGame(){
     let username = document.getElementById("newGame").value
     let ruleset = document.getElementById("ruleset").value
@@ -60,9 +118,11 @@ function toNewGame(){
 }
 xmlhttp.open("GET","http://35.231.124.196/home?"+queryObjectToString({uname:uname.value}));
 xmlhttp.send();
-
+*/
 function toGame(){
-    document.getElementById("list").onclick = function() { location.href = "game.html"}
+	location.href = "game.html";
+	//window.open("http://35.231.124.196/game.html");
+    //document.getElementById("list").onclick = function() { location.href = "game.html"}
 }
 
 
@@ -80,3 +140,8 @@ function createPopup() {
 	}
 }
 
+function queryObjectToString(query) {
+    let properties = Object.keys(query);
+    let arrOfQuesryStrings = properties.map(prop => prop+"="+query[prop]);
+    return(arrOfQuesryStrings.join('&'));
+ }
