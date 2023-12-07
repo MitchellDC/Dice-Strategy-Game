e945f7dc2fae9da3b06648295dbc1af3335164e4
Mitchell DeCesare <mitchelldc18@gmail.com>
2023-12-07 17:19:31 -0500

    admin changes

--- public_html/js/admin.js
+++ public_html/js/admin.js
@@ -12,3 +12,28 @@ function editPopup() {
 	}
 }
 
+function createPopup() {
+	let modal = document.getElementById("createmodal"); 
+	let span = document.getElementsByClassName("close");
+	modal.style.display = "block";
+	span[0].onclick = function() {
+		modal.style.display = "none";
+	}
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+}
+
+
+
+document.addEventListener("DOMContentLoaded" , function () {
+	let signoutB = document.getElementById("signout");
+	signoutB.addEventListener("click", signout); 
+
+});
+
+function signout() {
+	location.href = "login.html"; 
+}
