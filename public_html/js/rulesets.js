50854948634dbd593d2ae8ef9738ec5e2583b223
Aaron Barbiero <124076675+nicespiced@users.noreply.github.com>
2023-12-08 17:17:12 -0500

    Update rulesets.js

--- public_html/js/rulesets.js
+++ public_html/js/rulesets.js
@@ -1,11 +1,11 @@
-function toHome() {
+function toHome() { 
     document.getElementById("home").onclick = function () { location.href = "home.html"}
 }
 
 function toLogin(){
     document.getElementById("signout").onclick = function() { location.href = "login.html"}
 }
-
+ 
 function toRules() {
     document.getElementById("rules").onclick = function () { location.href = "rulesets.html"}
 }
@@ -15,16 +15,15 @@ function showRules() {
 }
 
 
-function displayRulesets(){
+let ruleTable = document.getElementById("ruleTable");
+console.log("outsideRuletable: "+ ruleTable)
 
+
+function displayRulesets(){
 
 	// takes the rulesets table from the html
 	let ruleTable = document.getElementById("ruleTable");
-<<<<<<< HEAD
-
-=======
     console.log("ruletable: "+ ruleTable)
->>>>>>> e060db734b62164d406ad71a3fb1a6461988186e
 	let xmlhttp = new XMLHttpRequest();
 	xmlhttp.onerror = function(){alert("Error!")};
 	xmlhttp.onload = function(){
@@ -32,38 +31,22 @@ function displayRulesets(){
 		else{
 
 			let resp = JSON.parse(this.responseText); // takes the response from the server in variable resp
-			console.log(resp)
+			console.log("resp: "+ resp)
 			if(resp.length>0){
-<<<<<<< HEAD
-				document.getElementById("noRules").style.display="none"; // hides the table that says no rulesets if there are no active rulesets
-				let column = 0
-                for(Rule in resp){ // every ruleset in the response is taken
-                    if (column == 4) {
-                        column = 0
-                        newRow = ruleTable.insertRow(); // new row is made
-=======
 				document.getElementById("noRulesets").style.display="none"; // hides the table that says no rulesets if there are no active rulesets
-				let column = 0
-				newRow = ruleTable.insertRow(); // new row is made
-                for(Rule in resp){ // every ruleset in the response is taken
-                    if (column == 4) {
-                        column = 0
-						newRow
->>>>>>> e060db734b62164d406ad71a3fb1a6461988186e
-                    }
+				console.log("ruletable: " + ruleTable)
 
-					ruleColumn = newRow.insertCell(column); // variable for the second column of the new row
+				for(let i = 0; i < resp.length; i++) {
+					
+					newRow = ruleTable.insertRow() // maybe i+1
 
-					ruleColumn.innerHTML = resp[Rule].Ruleset_name //displays the ruleset (will change once the rulesets are applied)
+					// variable for the second column of the new row
 
-                    let ruleID = resp[Rule].Rule_ID;
-<<<<<<< HEAD
-					ruleColumn.classList.add("ongoing"); //assigns class to the columns
-=======
-					ruleColumn.classList.add("details"); //assigns class to the columns
->>>>>>> e060db734b62164d406ad71a3fb1a6461988186e
-					ruleColumn.id = ruleID; //assigns id to the first column
+					newRow.innerHTML = resp[i].Ruleset_name //displays the ruleset (will change once the rulesets are applied)
+					newRow.classList.add("details"); //assigns class to the columns
 					//alert(resp[game].Game_ID)
+					let ruleID = resp[i].Rule_ID;
+					newRow.id = ruleID; //assigns id to the first column
 
 					/*
 					oppColumn.addEventListener("click", function(){
@@ -74,23 +57,22 @@ function displayRulesets(){
 
 					// anonymous function where opponent is different at each iteration of the loop
 
-<<<<<<< HEAD
-                        column += column
-=======
 					(function (ruleID) {
-						ruleColumn.addEventListener("click", function () {
-							 alert(ruleID);
-							document.getElementById("ruleset").classList.toggle("active")
+						newRow.addEventListener("click", function () {
+
+						alert(ruleID)
+
+						document.getElementById(ruleID).classList.toggle("active")
 						});
 					})(ruleID)
 
-                    column += column
->>>>>>> e060db734b62164d406ad71a3fb1a6461988186e
 				}
+
+					
 			}
 			else{
 				document.getElementById("noRules").style.display="block"; //another table that says no active rulesets if there are no rulesets made
-				document.getElementById("noRules").innerHTML="<b>NO ACTIVE RULESETS</b>"
+				//document.getElementById("noRules").innerHTML="<b>NO ACTIVE RULESETS</b>"
 				ruleTable.style.display='none'; //table is hidden if there are no active rulesets
 			}
 
@@ -100,8 +82,4 @@ function displayRulesets(){
 	xmlhttp.open("GET","http://104.196.1.169/rules");
 	xmlhttp.send();
 }
-<<<<<<< HEAD
 displayRulesets();
-=======
-displayRulesets();
->>>>>>> e060db734b62164d406ad71a3fb1a6461988186e
