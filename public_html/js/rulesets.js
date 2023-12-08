8af57a7269395ef882d1e9955b0f7ebcef74dcf3
Aaron Barbiero <124076675+nicespiced@users.noreply.github.com>
2023-12-08 17:24:37 -0500

    Update rulesets.js

--- public_html/js/rulesets.js
+++ public_html/js/rulesets.js
@@ -11,7 +11,7 @@ function toRules() {
 }
 
 function showRules() {
-    document.getElementById("ruleset").classList.toggle("active")
+    document.getElementById("rulePopup").classList.toggle("active")
 }
 
 
@@ -59,10 +59,8 @@ function displayRulesets(){
 
 					(function (ruleID) {
 						newRow.addEventListener("click", function () {
-
 						alert(ruleID)
-
-						document.getElementById(ruleID).classList.toggle("active")
+						document.getElementById("rulePopup").classList.toggle("active")
 						});
 					})(ruleID)
 
